335efafb491b83c08a57ecaf6ee4fe0eeceb04ae
Onur Yiğit <onuryigit54@yaani.com>
2021-01-19 01:00:50 +0300

    Add project files.

---
+++ ClassMetotDemo2/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo2
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " adlı müşteri eklenmiştir");
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " adlı müşteri listelenmiştir");
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " adlı müşteri silinmiştir");
+        }
+    }
+}
---
+++ ClassMetotDemo2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassMetotDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Onur";
+            musteri1.Soyad = "Yiğit";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Defne Bilge";
+            musteri2.Soyad = "Yiğit";
+
+            //Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            Console.WriteLine("-------------------------------");
+            musteriManager.Listele(musteri1);
+            musteriManager.Listele(musteri2);
+            Console.WriteLine("-------------------------------");
+            musteriManager.Sil(musteri1);
+            musteriManager.Sil(musteri2);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
